5ad159eebb6c8dbc9110da930093c23b29dd146e
Salvatore Di Genua <saldigenua@gmail.com>
2026-07-14 14:44:50 +0200

    add transitions

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,7 @@ import {
   isDevMode,
   provideBrowserGlobalErrorListeners,
 } from '@angular/core';
-import { provideRouter, withInMemoryScrolling } from '@angular/router';
+import { provideRouter, withInMemoryScrolling, withViewTransitions } from '@angular/router';
 import { provideTransloco } from '@jsverse/transloco';
 import { provideTranslocoPersistLang } from '@jsverse/transloco-persist-lang';
 import { provideRequestLock } from 'ngx-request-lock';
@@ -20,6 +20,7 @@ export const appConfig: ApplicationConfig = {
         scrollPositionRestoration: 'top',
         anchorScrolling: 'enabled',
       }),
+      withViewTransitions()
     ),
     // Registers HttpClient with the ngx-request-lock functional interceptor.
     // Do NOT add a separate provideHttpClient() call - it would override this.
