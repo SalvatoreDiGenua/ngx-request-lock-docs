4cd1c9dd1f0cba3d60e6de1811b1e1bc11dd781d
Salvatore Di Genua <saldigenua@gmail.com>
2026-08-03 22:39:13 +0200

    feat(angular_ssr): enhance language storage handling with platform detection

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,6 +2,7 @@ import {
   ApplicationConfig,
   inject,
   isDevMode,
+  PLATFORM_ID,
   provideAppInitializer,
   provideBrowserGlobalErrorListeners,
 } from '@angular/core';
@@ -18,6 +19,26 @@ import { routes } from './app.routes';
 import { TranslocoHttpLoader } from './core/i18n/transloco-loader';
 import { provideClientHydration } from '@angular/platform-browser';
 import { SeoRouterService } from './core/services/seo-router-service';
+import { isPlatformBrowser } from '@angular/common';
+
+class NoopStorage implements Storage {
+  length = 0;
+  clear(): void {
+    //
+  }
+  getItem(): string | null {
+    return null;
+  }
+  key(): string | null {
+    return null;
+  }
+  removeItem(): void {
+    //
+  }
+  setItem(): void {
+    //
+  }
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -45,7 +66,11 @@ export const appConfig: ApplicationConfig = {
       loader: TranslocoHttpLoader,
     }),
     provideTranslocoPersistLang({
-      storage: { useValue: localStorage },
+      storage: {
+        useFactory: (platformId: object) =>
+          isPlatformBrowser(platformId) ? localStorage : new NoopStorage(),
+        deps: [PLATFORM_ID],
+      },
       storageKey: 'ngx-request-lock-docs.language',
     }),
     provideClientHydration(),
